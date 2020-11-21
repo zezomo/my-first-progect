d4e5918a3bdc358c0467a29ea8193ca1181925b9
zezomo <72624982+zezomo@users.noreply.github.com>
2020-11-21 12:49:11 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+const navitems = document.querySelector(".navitems");
+const myarr = document.querySelectorAll(".section");
+const sections = Array.from(myarr);
+const fragment = document.createDocumentFragment();
+// Navigation is built dynamically.
+for (const section of sections) {
+  const show = document.createElement("a");
+  show.textContent = `${section.id}`;
+  show.setAttribute("class", `${section.id} order disappear`);
+  show.addEventListener("click", function onclick() {
+    show.setAttribute("href", `#${section.id}`);
+  });
+  fragment.appendChild(show);
+}
+navitems.appendChild(fragment);
+// Highlight viewed section while scrolling.
+const options = {
+  threshold: 0.7,
+};
+let observer = new IntersectionObserver(navCheck, options);
+
+function navCheck(entries) {
+  entries.forEach((entry) => {
+    const className = entry.target.id;
+    const choise = document.querySelector(`.${className}`);
+    if (entry.isIntersecting) {
+      choise.classList.add("mark");
+    } else {
+      choise.classList.remove("mark");
+    }
+  });
+}
+sections.forEach((section) => {
+  observer.observe(section);
+});
+function toggleclass() {
+  let links = document.querySelector(".navitems");
+  links.classList.toggle("togglecls");
+}
